949fe100b635f8d272a279ce29b38f74035dd43d
Nick YOOO <outgrowthr@gmail.com>
2023-06-02 10:40:14 +0900

    Refactor: Search 로직 -> 변경 Feat: js 모듈화를 위해 main.js 생성

---
+++ modules/main.js
@@ -0,0 +1,77 @@
+import { URL } from "./fetchurl.js";
+import { OPTIONS } from "./option.js";
+import { makeList } from "./makelist.js";
+
+
+// fetch
+const fetchMovie = async function () {
+  const response = await fetch(URL, OPTIONS);
+  const data = await response.json();
+  const movies = data.results;
+  return movies;
+};
+
+// card list
+async function cardList(arr) {
+  const movies = await fetchMovie();
+  if (!arr) {
+    makeList(movies);
+  } else {
+    const cardList = document.getElementById("cardList");
+    cardList.innerHTML = "";
+    makeList(arr);
+  }
+}
+cardList();
+
+//  Search movie
+const $frm = document.search;
+$frm.addEventListener("submit", findMovie);
+async function findMovie(e) {
+  e.preventDefault();
+  const movies = await fetchMovie();
+
+  const userInput = $frm.searchInput.value.toLowerCase();
+  const userMovieTitle = userInput.replace(/(\s*)/g, "");
+  const matchMovies = movies.filter((item) => {
+    let titles = item.title.toLowerCase().replace(/(\s*)/g, "");  //split(' ').join('')
+    return titles.includes(userMovieTitle);
+  });
+
+  if (matchMovies.length === 0) {
+    alert("검색어를 확인해 주세요.");
+  } else {
+    cardList(matchMovies);
+  }
+}
+
+//  click logo
+const img = document.querySelector("header img");
+img.addEventListener("click", () => {
+  window.location.reload();
+});
+
+//  Click card
+const clickCard = document.getElementById("cardList");
+clickCard.addEventListener("click", (e) => {
+  let card = e.target.parentNode;
+  if (card.className !== "movie-card") {
+    card = card.parentNode;
+  }
+
+  const contentId = card.getAttribute("id");
+  if (!contentId) {
+    return false;
+  }
+  alert("영화 id: " + contentId);
+});
+
+
+
+// Enter Key
+// function press(f){
+//   if(f.keyCode == "enter"){ //javascript에서는 13이 enter키를 의미
+//       search.submit();
+//   };
+// }
+// key code 메소드 단점
