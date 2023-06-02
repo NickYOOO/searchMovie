206325986aaec429405be8d5385f6efb0f5f5c99
Nick YOOO <outgrowthr@gmail.com>
2023-06-02 10:19:23 +0900

    Feat: js 모듈화를 위해 option.js 생성, TMDB api정보를 export 해줌

---
+++ modules/option.js
@@ -0,0 +1,7 @@
+export const OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNmM2ODZkMDdhMTcxNDUyZWUyYzM2MGUwODFjNjAxMyIsInN1YiI6IjY0NzRjMGFhNWNkMTZlMDBiZjEyNDQ2OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qgYpZS6S6GHYU8eq2rRevqoZz5g80tZ7hZ5KJ3soHVU'
+    },
+};
